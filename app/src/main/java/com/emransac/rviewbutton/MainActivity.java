237ac4f73e6aade08026050fa2956cd880d2cfeb
EmaranSistemas <sistemas.emaransac@gmail.com>
2023-06-05 16:59:47 -0500

    now this program include id inventario pedido casi todo

--- app/src/main/java/com/emransac/rviewbutton/MainActivity.java
+++ app/src/main/java/com/emransac/rviewbutton/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements mAdapter.itemClic
 
 
         for(int i=0;i<5;i++){
-            producto= new Producto(Integer.toString(i),"Nombre producto "+i,"0","0","0","https://sibarita.pe/wp-content/uploads/2021/07/SIB008.jpg");
+            producto= new Producto(Integer.toString(i),"Canela "+i,"0","0","0","https://sibarita.pe/wp-content/uploads/2021/07/SIB008.jpg");
             productArrayList.add(producto);
         }
 
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements mAdapter.itemClic
     }
 
     @Override
-    public void onTextInputClicked(String nombre, String stock, String pedido) {
-        Toast.makeText(MainActivity.this,"Nombre: "+nombre+" Stock: "+stock,Toast.LENGTH_SHORT).show();
+    public void onTextInputClicked(String nombre, String inventario, String pedido) {
+        Toast.makeText(MainActivity.this,"N: "+nombre+" I: "+inventario+" P: "+pedido,Toast.LENGTH_SHORT).show();
     }
 }
